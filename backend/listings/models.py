e4747826a81aa9a37ab7eb06e6c96be64f44d304
Connor VanTress <11cvantress@gmail.com>
2024-10-07 13:35:11 -0500

    Updated listing model. Now allows for logic that determines if a listing should be displayed or not

--- backend/listings/models.py
+++ backend/listings/models.py
@@ -6,10 +6,12 @@ class Listing(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_created = models.DateTimeField(auto_now_add=True)
-
-    # Reference to User model
+    # Reference to User model 
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     is_active = models.BooleanField(default=True)
+    # Determines whether a listing is allowed to be viewed or not. i.e. does it have at least two 
+    # images associated with it
+    is_visible = models.BooleanField(default=False)
     category = models.CharField(max_length=100, blank=True, null=True)  
     
     def __str__(self):
@@ -19,7 +21,16 @@ class Listing(models.Model):
 class ListingImage(models.Model):
     listing = models.ForeignKey(Listing, related_name='images', on_delete=models.CASCADE)
     # Images will be stored in the media/listings folders
-    image = models.ImageField(upload_to='listings/')  
+    image = models.ImageField(upload_to='listings/')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        # Check the number of images associated with the listing.
+        # Set to true if there are at least two
+        if self.listing.images.count() >= 2:
+            self.listing.is_visible = True
+            self.listing.save(update_fields=['is_visible'])  
 
     def __str__(self):
         return f"Image for {self.listing.title}"
