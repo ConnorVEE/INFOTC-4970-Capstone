8506e895d7f1349b7a84ae7886d72a094256c03d
Connor VanTress <11cvantress@gmail.com>
2024-10-07 13:56:34 -0500

    Added API endpoint that allows for listing creation

--- backend/listings/views.py
+++ backend/listings/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-from rest_framework import generics
-#from .models import Listing
-#from .serializers import ListingSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-'''
-class ListingList(generics.ListCreateAPIView):
-    queryset = Listing.objects.all()
-    serializer_class = ListingSerializer
-    
-class ListingDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Listings.objects.all()
-    serializer_class = ListingSerializer
-'''
+# Local application imports (internal models, serializers, etc.)
+from .models import Listing
+from .serializers import ListingSerializer, ListingImageSerializer
+
+
+class ListingCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # Assign the authenticated user to the listing
+        data = request.data.copy()
+        data['user'] = request.user.id 
+        
+        # Create the listing using the serializer
+        serializer = ListingSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()  # Save listing to the database
+            return Response({ "message": "Listing created successfully.", "listing": serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
