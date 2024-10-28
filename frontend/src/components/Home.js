7fc299e510163c0b11d8e39216c6a7213ef093c1
Erin <eefnmc@umsystem.edu>
2024-10-28 16:53:37 -0500

    added logo

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Home = () => {
     return (
         <div className="home-container">
+            <Navbar /> {/* Include the Navbar here */}
             <div>
                 <h1>Welcome to the Home Page!</h1>
             </div>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Make sure to import Link from react-router-dom
+import logo from '../components/MMLogo.png'; // Adjust the path as necessary
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+       <div className="logo-container">
+          <img src={logo} alt="Mizzou Marketplace Logo"  style={{ width: '200px', height: 'auto' }} className="navbar-logo" />
+      </div>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">Cart</Link></li>
         <li><Link to="/contact">Products</Link></li>
         <li><Link to="/login">Account</Link></li>
