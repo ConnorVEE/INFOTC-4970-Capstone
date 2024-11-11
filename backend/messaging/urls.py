69406fbb8e5f7360194c3f44744b50ff2668316a
owenbartle <obbartle01@gmail.com>
2024-11-11 14:46:14 -0600

    update messaging routes

--- backend/messaging/urls.py
+++ backend/messaging/urls.py
@@ -4,8 +4,8 @@ from .views import ConversationView, MessageView
 
 urlpatterns = [
     # URL for listing all conversations and creating a new one
-    path('conversations/', ConversationView.as_view(), name='conversation-list-create'),
+    path('', ConversationView.as_view(), name='conversation-list-create'),
     
     # URL for listing all messages in a conversation and creating a new message
-    path('conversations/<int:conversation_id>/messages/', MessageView.as_view(), name='message-list-create')
+    path('<int:conversation_id>/messages/', MessageView.as_view(), name='message-list-create')
 ]
