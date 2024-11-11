a9add3dbcfba7e74e432ceebe9ff0b9bdbf7b6df
owenbartle <obbartle01@gmail.com>
2024-11-11 16:00:22 -0600

    made migrations and fixed up db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,6 +50,7 @@ SIMPLE_JWT = {
 # Application definition
 
 INSTALLED_APPS = [ #added api, rest_framework, and corsheaders
+    "users",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,7 +59,6 @@ INSTALLED_APPS = [ #added api, rest_framework, and corsheaders
     'django.contrib.staticfiles',
     # Modules
     "listings", 
-    "users",
     "messaging",
     "transactions",
 
@@ -69,6 +69,7 @@ INSTALLED_APPS = [ #added api, rest_framework, and corsheaders
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,7 +77,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
